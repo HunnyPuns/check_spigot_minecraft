9ef47b76a2c3ee9987148e4669866e8849992aac
Michael Bellerue <michael.bellerue@reinforceme.com>
2021-01-05 03:02:48 -0600

    Updating readme and submitting check_spigot_minecraft.py

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # check_spigot_minecraft
 A Nagios plugin for monitoring Spigot based Minecraft servers via RCON.
+
+# Installation
+Not much to note about installation other than this script does not directly handle RCON. You do have the option to specify an RCON client. In my case I used [mcrcon](https://github.com/Tiiffi/mcrcon.git), which is why I have it listed in the help section.
+
+# Usage
+
+```
+usage: check_spigot_minecraft.py [-h] [-H HOST] -p PASSWORD [-P PORT] [-l LIST] [-m MCRCON] [-w WARNING] [-c CRITICAL] {tps} ...
+
+positional arguments:
+  {tps}                 sub-command help
+    tps                 Monitor TPS and/or memory utilization
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -H HOST, --host HOST  IP address of the Minecraft server
+  -p PASSWORD, --password PASSWORD
+                        rcon password for the Minecraft server
+  -P PORT, --port PORT  rcon port for the Minecraft server
+  -l LIST, --list LIST  List the number of players on the server
+  -m MCRCON, --mcrcon MCRCON
+                        Path to the mcrcon binary, including binary name.
+  -w WARNING, --warning WARNING
+                        Set the warning threshold
+  -c CRITICAL, --critical CRITICAL
+                        Set the critical threshold
+```
+
+```
+usage: check_spigot_minecraft.py tps [-h] [-k {tps,memory}]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -k {tps,memory}, --key {tps,memory}
+                        Set the key metric. e.g. tps or memory
+```
+
+Host specifies the host that you want to check.
+
+Password specifies the RCON password for the host.
+
+Port specifies the port that RCON is listening on, default is 25575
+
+List will list the number of users currently connected to the server.
+
+Mcrcon will specify the location of the mcrcon binary
+
+Warning and Critical will set thresholds for user count, TPS, or memory utilization. These are optional if you just want to track the usage. They will output in Nagios format, with performance data.
+
+The tps positional argument lets you specify whetehr you want to check the TPS on the server, or the memory utilization.
+
+# Things to Note
+When monitoring TPS, Spigot Minecraft, and by proxy mcrcon, will return 1 minute, 5 minute, and 15 minute TPS values. In v1.0 of the script, all three of these metrics will be reported back, and tracked in the performance data, however the warning and critical thresholds only apply to the 15 minute TPS value. In the future I'd like to make an option to just specify which interval you'd like to monitor, but in the meantime if you'd like to change that, line 124 starts the check against the warning/critical thresholds. I do break out all three intervals into their own variables, so it can be changed pretty easily.
+
+Also remember when monitoring TPS that a lower value is worse. Therefore your warning threshold should be a higher number than your critical threshold.
---
+++ check_spigot_minecraft.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python3
+
+#Version 1.0
+
+#
+# check_minecraft.py created by HunnyPuns on January 4th, 2021
+# GitHub: https://github.com/HunnyPuns/
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# GPL v3: https://www.gnu.org/licenses/gpl-3.0.html
+#
+
+import argparse
+import subprocess
+import sys
+
+returnusercount = -1
+returnmaxcount = -1
+
+returntps = ''
+returnmem = ''
+
+#0 = OK, 1 = WARNING, 2 = CRITICAL, 3 and greater = UNKNOWN
+#The script must prove that everything is OK, else it is UNKNOWN.
+status = 3
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-H',
+                    '--host',
+                    required=(),
+                    type=str,
+                    help='IP address of the Minecraft server')
+parser.add_argument('-p',
+                    '--password',
+                    required=True,
+                    type=str,
+                    help='rcon password for the Minecraft server')
+parser.add_argument('-P',
+                    '--port',
+                    required=False,
+                    type=str,
+                    default='25575',
+                    help='rcon port for the Minecraft server')
+parser.add_argument('-l',
+                    '--list',
+                    required=False,
+                    type=bool,
+                    help='List the number of players on the server')
+parser.add_argument('-m',
+                    '--mcrcon',
+                    required=False,
+                    type=str,
+                    default='/home/hunnypuns/Documents/devcorner/check_minecraft/mcrcon',
+                    help='Path to the mcrcon binary, including binary name.')
+parser.add_argument('-w',
+                        '--warning',
+                        required=False,
+                        type=int,
+                        help='Set the warning threshold')
+parser.add_argument('-c',
+                        '--critical',
+                        required=False,
+                        type=int,
+                        help='Set the critical threshold')
+
+subparsers = parser.add_subparsers(help='sub-command help', dest='command')
+
+parser_tps = subparsers.add_parser('tps',
+                                    help='Monitor TPS and/or memory utilization')
+parser_tps.add_argument('-k',
+                        '--key',
+                        required=False,
+                        type=str,
+                        choices=['tps','memory'],
+                        help='Set the key metric. e.g. tps or memory')
+
+args = parser.parse_args(sys.argv[1:])
+
+resultstring = 'Nothing changed the result string'
+
+if (args.list is None and args.key == 'memory'):
+    tpsdata = subprocess.getoutput('{0} -H {1} -P {2} -p {3} -c "tps"'.format(args.mcrcon,
+                                                                            args.host,
+                                                                            args.port,
+                                                                            args.password))
+
+    returnmem = tpsdata.split("\n")[1]
+    usedmem = int(returnmem.split('/')[0].split(' ')[3])
+    maxmem = int(returnmem.split('/')[1].split(' ')[3])
+    resultstring = 'Memory: {0} used of {1} max | used_mem={0}; max_mem={1}'.format(usedmem, maxmem)
+
+    if (args.critical is not None and usedmem > args.critical):
+        status = 2
+    elif (args.warning is not None and usedmem > args.warning):
+        status = 1
+    else:
+        status = 0
+
+
+elif (args.list is None and args.key == 'tps'):
+    tpsdata = subprocess.getoutput('{0} -H {1} -P {2} -p {3} -c "tps"'.format(args.mcrcon,
+                                                                            args.host,
+                                                                            args.port,
+                                                                            args.password))
+
+    if (args.key == 'tps' and args.critical is not None and args.warning is not None):
+        if (args.warning < args.critical):
+            resultstring = 'When alerting on TPS, lower is worse. Warning should be a higher number than critical.'
+            status = 3
+
+            print(resultstring)
+            sys.exit(status)
+
+    returntps = tpsdata.split("\n")[0]
+    tps1m = float(returntps.split(':')[1].split(' *')[1][0:4])
+    tps5m = float(returntps.split(':')[1].split(' *')[2][0:4])
+    tps15m = float(returntps.split(':')[1].split(' *')[3][0:4])
+    resultstring = 'tps times were {0}, {1}, {2}|tps_1minute={0}; tps_5minute={1}; tps_15minute={2}'.format(tps1m, tps5m, tps15m)
+
+    if (args.critical is not None and tps15m < args.critical):
+        status = 2
+    elif (args.warning is not None and tps15m < args.warning):
+        status = 1
+    else:
+        status = 0
+
+elif (args.list is True):
+    userlist = subprocess.getoutput('{0} -H {1} -P {2} -p {3} -c "list"'.format(args.mcrcon,
+                                                                            args.host,
+                                                                            args.port,
+                                                                            args.password))
+    returnusercount = userlist.split(" ")[2]
+    returnmaxcount = userlist.split(" ")[7]
+    status = 0
+    resultstring = '{0} users of {1} connected.|current_users={0}; max_users={1}'.format(returnusercount, returnmaxcount)
+
+print(resultstring)
+sys.exit(status)
